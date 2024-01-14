1f9311616badb5622cf5a27818ab10f9673cb4f9
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2024-01-14 14:28:04 +0300

    Add swiftgen for assets and colors

--- SwiftGenTutorial/SwiftGenTutorial/swiftgen/assets.generated.swift
+++ SwiftGenTutorial/SwiftGenTutorial/swiftgen/assets.generated.swift
@@ -1,1 +1,16 @@
+// 🤖 This file was code-generated. Do not edit!!!
 
+import UIKit
+
+// MARK: - Images
+public extension UIImage {
+  static let facebook: UIImage = UIImage(named: "facebook")!
+  static let google: UIImage = UIImage(named: "google")!
+}
+
+// MARK: - Colors
+public extension UIColor {
+  static let accentColor: UIColor = UIColor(named: "AccentColor")!
+  static let customColor: UIColor = UIColor(named: "custom-Color")!
+  static let `default`: UIColor = UIColor(named: "default")!
+}
